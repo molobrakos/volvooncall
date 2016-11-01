0c3c991f31d11f446fa503c5eadbf4c7c877af4f
Erik Eriksson <molobrakos@users.noreply.github.com>
2016-11-01 12:49:03 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="volvooncall",
+      version="0.1.1",
+      description="Communicate with VOC",
+      url="https://github.com/molobrakos/volvooncall",
+      license="",
+      author="Erik",
+      author_email="Erik",
+      py_modules=["volvooncall"],
+      provides=["volvooncall"],)
