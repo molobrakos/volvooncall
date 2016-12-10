9a05e89347e8661c1fc7584179b7e02a150e6f5a
Erik Eriksson <molobrakos@users.noreply.github.com>
2016-12-10 15:21:39 +0100

    no need for yaml. don't return state in update(). strings fixup

--- volvooncall.py
+++ volvooncall.py
@@ -12,14 +12,15 @@ from requests.compat import urljoin
 _LOGGER = logging.getLogger(__name__)
 
 SERVICE_URL = 'https://vocapi.wirelesscar.net/customerapi/rest/v3.0/'
-HEADERS = {"X-Device-Id": "Device",
-           "X-OS-Type": "Android",
-           "X-Originator-Type": "App"}
+HEADERS = {'X-Device-Id': 'Device',
+           'X-OS-Type': 'Android',
+           'X-Originator-Type': 'App'}
 
 TIMEOUT = timedelta(seconds=5)
 
 
 class Connection():
+
     """Connection to the VOC server."""
 
     def __init__(self, username, password):
@@ -28,61 +29,64 @@ class Connection():
         self._session.headers.update(HEADERS)
         self._session.auth = (username,
                               password)
-        self._state = None
+        self._state = {}
 
     def query(self, ref, rel=SERVICE_URL):
         """Perform a query to the online service."""
         url = urljoin(rel, ref)
-        _LOGGER.debug("Request for %s", url)
+        _LOGGER.debug('Request for %s', url)
         res = self._session.get(url, timeout=TIMEOUT.seconds)
         res.raise_for_status()
-        _LOGGER.debug("Received %s", res.json())
+        _LOGGER.debug('Received %s', res.json())
         return res.json()
 
     def update(self, reset=False):
         """Update status."""
         try:
-            _LOGGER.info("Updating")
+            _LOGGER.info('Updating')
             if not self._state or reset:
-                _LOGGER.info("Querying vehicles")
-                user = self.query("customeraccounts")
+                _LOGGER.info('Querying vehicles')
+                user = self.query('customeraccounts')
                 self._state = {}
-                for vehicle in user["accountVehicleRelations"]:
+                for vehicle in user['accountVehicleRelations']:
                     rel = self.query(vehicle)
-                    vehicle = rel["vehicle"] + '/'
-                    state = self.query("attributes", vehicle)
+                    vehicle = rel['vehicle'] + '/'
+                    state = self.query('attributes', vehicle)
                     self._state.update({vehicle: state})
-            _LOGGER.debug("Updating ")
-            for vehicle in self._state:
-                status = self.query("status", vehicle)
-                position = self.query("position", vehicle)
-                vehicle = self._state[vehicle]
-                vehicle.update(status)
-                vehicle.update(position)
-            _LOGGER.debug("State: %s", self._state)
-            return True, self._state.values()
+            for vin, vehicle in self._state.items():
+                self._state[vin].update(
+                    self.query('status', vin))
+                self._state[vin].update(
+                    self.query('position', vin))
+                _LOGGER.debug('State: %s', self._state)
+            return True
         except requests.exceptions.RequestException as error:
-            _LOGGER.error("Could not query server: %s", error)
-            return False, self._state.values()
+            _LOGGER.error('Could not query server: %s', error)
 
-if __name__ == "__main__":
+    @property
+    def state(self):
+        """Return state."""
+        return list(self._state.values())
+
+
+def main():
+    """Command line interface."""
     from os import path
     from sys import argv
     from pprint import pprint
     logging.basicConfig(level=logging.INFO)
 
-    def credentials():
-        """Return user, pass."""
-        if len(argv) == 3:
-            return argv[1:]
-        try:
-            from yaml import safe_load as load_yaml
-            with open(path.join(path.dirname(argv[0]),
-                                ".credentials.yaml")) as config:
-                return load_yaml(config)
-        except ImportError:
-            exit(-1)
+    if len(argv) == 3:
+        credentials = argv[1:]
+    else:
+        with open(path.join(path.dirname(argv[0]),
+                            '.credentials.conf')) as config:
+            credentials = dict(x.split(": ")
+                               for x in config.read().strip().splitlines())
+    connection = Connection(**credentials)
+    if connection.update():
+        pprint(connection.state)
 
-    res, value = Connection(**credentials()).update()
-    if res:
-        pprint(list(value))
+
+if __name__ == '__main__':
+    main()
