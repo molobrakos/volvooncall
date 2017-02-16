0a1120cc98298a001f689b069075270d25286bd5
Erik <molobrakos@users.noreply.github.com>
2017-02-16 10:59:58 +0100

    fix

--- volvooncall.py
+++ volvooncall.py
@@ -10,7 +10,7 @@ import re
 from requests import Session, RequestException
 from requests.compat import urljoin
 
-__version__ = '0.1.u10'
+__version__ = '0.1.10'
 
 _LOGGER = logging.getLogger(__name__)
 
