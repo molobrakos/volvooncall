72ce9620bef3607d38e407b10568a238175ee8b0
Erik Eriksson <molobrakos@users.noreply.github.com>
2017-11-10 18:27:32 +0100

    Bump version: 0.3.3 → 0.4.0

--- volvooncall.py
+++ volvooncall.py
@@ -11,7 +11,7 @@ import re
 from requests import Session, RequestException
 from requests.compat import urljoin
 
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 
 _LOGGER = logging.getLogger(__name__)
 
