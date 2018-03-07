bdbefdc32a5910cc6cbf00a18df7f94917ef1dcb
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-03-07 17:59:39 +0100

    Fix name

--- volvooncall.py
+++ volvooncall.py
@@ -43,12 +43,12 @@ def _obj_parser(obj):
     return obj
 
 
-def json_serial(obj):
+def json_serialize(obj):
     """JSON serializer for objects not serializable by default json code"""
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
+    raise TypeError("Type %s not serializable" % type(obj))
 
 
 class Connection(object):
