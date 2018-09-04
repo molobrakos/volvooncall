e706989ac290e7c4e7759f45597cd4c8c1e38fd1
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-09-04 12:08:11 +0200

    Extracted utility functions

--- volvooncall.py
+++ volvooncall.py
@@ -3,7 +3,7 @@
 """Communicate with VOC server."""
 
 import logging
-from datetime import timedelta, date, datetime
+from datetime import timedelta, date
 from functools import partial
 from sys import argv, version_info
 from os import environ as env
@@ -11,11 +11,11 @@ from os.path import join, dirname, expanduser
 from itertools import product
 from json import dumps as to_json
 from collections import OrderedDict
-from base64 import b64encode
 
 from requests import Session, RequestException
 from requests.compat import urljoin
 
+from util import obj_parser, json_serialize, is_valid_path, find_path
 
 _ = version_info >= (3, 0) or exit('Python 3 required')
 
@@ -36,99 +36,6 @@ TIMEOUT = timedelta(seconds=30)
 
 
 _LOGGER.debug('Loaded %s version: %s', __name__, __version__)
-
-
-def _obj_parser(obj):
-    """Parse datetime."""
-    for key, val in obj.items():
-        try:
-            obj[key] = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S%z')
-        except (TypeError, ValueError):
-            pass
-    return obj
-
-
-def json_serialize(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError("Type %s not serializable" % type(obj))
-
-
-def find_path(src, path):
-    """Simple navigation of a hierarchical dict structure using XPATH-like syntax.
-
-    >>> find_path(dict(a=1), 'a')
-    1
-
-    >>> find_path(dict(a=1), '')
-    {'a': 1}
-
-    >>> find_path(dict(a=None), 'a')
-
-
-    >>> find_path(dict(a=1), 'b')
-    Traceback (most recent call last):
-    ...
-    KeyError: 'b'
-
-    >>> find_path(dict(a=dict(b=1)), 'a.b')
-    1
-
-    >>> find_path(dict(a=dict(b=1)), 'a')
-    {'b': 1}
-
-    >>> find_path(dict(a=dict(b=1)), 'a.c')
-    Traceback (most recent call last):
-    ...
-    KeyError: 'c'
-
-    """
-    if not path:
-        return src
-    if isinstance(path, str):
-        path = path.split('.')
-    return find_path(src[path[0]], path[1:])
-
-
-def is_valid_path(src, path):
-    """
-    >>> is_valid_path(dict(a=1), 'a')
-    True
-
-    >>> is_valid_path(dict(a=1), '')
-    True
-
-    >>> is_valid_path(dict(a=1), None)
-    True
-
-    >>> is_valid_path(dict(a=1), 'b')
-    False
-    """
-    try:
-        find_path(src, path)
-        return True
-    except KeyError:
-        return False
-
-
-def owntracks_encrypt(msg, key):
-    try:
-        from libnacl import crypto_secretbox_KEYBYTES as keylen
-        from libnacl.secret import SecretBox as secret
-        key = key.encode('utf-8')
-        key = key[:keylen]
-        key = key.ljust(keylen, b'\0')
-        msg = msg.encode('utf-8')
-        ciphertext = secret(key).encrypt(msg)
-        ciphertext = b64encode(ciphertext)
-        ciphertext = ciphertext.decode('ascii')
-        return ciphertext
-    except ImportError:
-        exit('libnacl missing')
-    except OSError:
-        exit('libsodium missing')
 
 
 class Connection(object):
@@ -157,7 +64,7 @@ class Connection(object):
             _LOGGER.debug('Request for %s', url)
             res = method(url, timeout=TIMEOUT.seconds)
             res.raise_for_status()
-            res = res.json(object_hook=_obj_parser)
+            res = res.json(object_hook=obj_parser)
             _LOGGER.debug('Received %s', res)
             return res
         except RequestException as error:
@@ -466,9 +373,9 @@ class Vehicle(object):
             self.vin)
 
     @property
-    def dashboard(self, scandinavian_miles=False):
-        from dashboard import Dashboard, CONF_SCANDINAVIAN_MILES
-        return Dashboard(self, dict(CONF_SCANDINAVIAN_MILES=scandinavian_miles))
+    def dashboard(self):
+        from dashboard import Dashboard
+        return Dashboard(self)
 
     @property
     def json(self):
