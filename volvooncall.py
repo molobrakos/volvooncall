563733ed6bffafcbc0777f6b67b8a3ec5ffe316f
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-09-12 08:36:53 +0200

    Bump version: 0.6.4 → 0.6.5

--- volvooncall.py
+++ volvooncall.py
@@ -20,7 +20,7 @@ from util import owntracks_encrypt  # noqa: F401
 
 _ = version_info >= (3, 0) or exit('Python 3 required')
 
-__version__ = '0.6.4'
+__version__ = '0.6.5'
 
 _LOGGER = logging.getLogger(__name__)
 
