d67fc3b3dfa391e4383f77a86f4f0b68892d883d
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-09-27 13:51:28 +0200

    Bump version: 0.6.8 → 0.6.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="volvooncall",
-      version='0.6.8',
+      version='0.6.9',
       description="Communicate with VOC",
       url="https://github.com/molobrakos/volvooncall",
       license="",
