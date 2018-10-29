99be9725b40ff4a29fffe8226062a228dff33553
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-10-29 12:29:38 +0100

    Bump version: 0.6.10 → 0.6.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="volvooncall",
-      version='0.6.10',
+      version='0.6.11',
       description="Communicate with VOC",
       url="https://github.com/molobrakos/volvooncall",
       license="",
--- volvooncall.py
+++ volvooncall.py
@@ -20,7 +20,7 @@ from util import owntracks_encrypt  # noqa: F401
 
 _ = version_info >= (3, 0) or exit('Python 3 required')
 
-__version__ = '0.6.10'
+__version__ = '0.6.11'
 
 _LOGGER = logging.getLogger(__name__)
 
