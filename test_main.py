d58c44a28f39bc7856722a130a540798af443a41
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-11-24 11:54:22 +0100

    Async tests

--- test_main.py
+++ test_main.py
@@ -1,91 +1,71 @@
+import asyncio
+import pytest
 from unittest.mock import patch
 from volvooncall import Connection
-import requests
+from asynctest import patch
 
-
-class MockResponse:
-    def __init__(self, json_data, status_code):
-        self.json_data = json_data
-        self.status_code = status_code
-
-    def json(self, **kwargs):
-        return self.json_data
-
-    def raise_for_status(self):
-        if self.status_code != 200:
-            raise requests.HTTPError(self.status_code)
-
-
-def mocked_requests_get(*args, **kwargs):
-    url = args[1]
-
+async def mocked_request(method, url, rel=None, **kwargs):
     if 'customeraccounts' in url:
-        return MockResponse(
-            {
-                'username': 'foobar',
-                'accountVehicleRelations': ['rel/1']
-            }, 200)
+        return {
+            'username': 'foobar',
+            'accountVehicleRelations': ['rel/1']
+        }
 
     if 'rel' in url:
-        return MockResponse(
-            {
-                'vehicle': 'vehicle/1'
-            }, 200)
+        return {
+            'vehicle': 'vehicle/1'
+        }
 
     if 'attributes' in url:
-        return MockResponse(
-            {
-                'registrationNumber': 'FOO123',
+        return {
+            'registrationNumber': 'FOO123',
 
-            }, 200)
+        }
 
     if 'status' in url:
-        return MockResponse(
-            {
-                'engineRunning': False,
-                'engineStartSupported': True,
-                'ERS': {'status': 'off'},
-            }, 200)
+        return {
+            'engineRunning': False,
+            'engineStartSupported': True,
+            'ERS': {'status': 'off'},
+        }
 
     if 'position' in url:
-        return MockResponse(
-            {
-            }, 200)
+        return {}
 
     if 'engine/start' in url:
-        return MockResponse(
-            {
-                'service': 'engine/start',
-                'status': 'Started'
-            }, 200)
+        return {
+            'service': 'engine/start',
+            'status': 'Started'
+        }
 
-    return MockResponse({
+    return {
         "error": "Unauthorized"
-    }, 401)
+    }
+
+@patch('volvooncall.Connection._request', side_effect=mocked_request)
+async def get_vehicle(req_mock):
+    async with Connection(username='', password='') as connection:
+        await connection.update()
+        req_mock.assert_called()
+        return next(connection.vehicles, None)
 
 
-@patch('requests.Session.request', side_effect=mocked_requests_get)
-def get_vehicle(req_mock):
-    connection = Connection(username='', password='')
-    connection.update()
-
-    return next(connection.vehicles, None)
-
-
-def test_basic():
-    vehicle = get_vehicle()
+@pytest.mark.asyncio
+async def test_basic():
+    vehicle = await get_vehicle()
     assert(vehicle)
     assert(vehicle.registration_number == 'FOO123')
-    print(vehicle.is_engine_running)
 
 
-def test_engine():
-    vehicle = get_vehicle()
+@pytest.mark.asyncio
+async def test_engine():
+    vehicle = await get_vehicle()
     assert(not vehicle.is_engine_running)
 
 
-def test_ers():
-    vehicle = get_vehicle()
+@pytest.mark.asyncio
+async def test_ers(event_loop):
+    vehicle = await get_vehicle()
     dashboard = vehicle.dashboard
     engines = [instrument
                for instrument in dashboard.instruments
@@ -98,36 +78,32 @@ def test_ers():
     assert(all(not engine.state for engine in engines))
 
 
-def get_started_vehicle():
+async def get_started_vehicle():
 
-    def mocked_requests_get_ers(*args, **kwargs):
-        url = args[1]
+    async def mocked_request_ers(method, url, rel=None, **kwargs):
         if 'status' in url:
-            return MockResponse(
-                {
-                    'engineRunning': False,
-                    'engineStartSupported': True,
-                    'ERS': {'status': 'on'},
-                }, 200)
-        return mocked_requests_get(*args, **kwargs)
+            return {
+                'engineRunning': False,
+                'engineStartSupported': True,
+                'ERS': {'status': 'on'},
+            }
+        return await mocked_request(method, url, rel, **kwargs)
 
-    vehicle = get_vehicle()
-
-    @patch('requests.Session.request', side_effect=mocked_requests_get_ers)
-    def start_engine(req_mock):
-        vehicle.start_engine()
-
-    start_engine()
-    return vehicle
+    vehicle = await get_vehicle()
+    with patch('volvooncall.Connection._request', side_effect=mocked_request_ers) as mock:
+        await vehicle.start_engine()
+        return vehicle
 
 
-def test_ers_start():
-    vehicle = get_started_vehicle()
+@pytest.mark.asyncio
+async def test_ers_start():
+    vehicle = await get_started_vehicle()
     assert(vehicle.is_engine_running)
 
 
-def test_ers_start_dashboard():
-    vehicle = get_started_vehicle()
+@pytest.mark.asyncio
+async def test_ers_start_dashboard():
+    vehicle = await get_started_vehicle()
     dashboard = vehicle.dashboard
     engines = [instrument
                for instrument in dashboard.instruments
