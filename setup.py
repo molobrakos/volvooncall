d356885772da92c8907846a144339e0526d8f7f7
Jonathan Keslin <github@jonathankeslin.com>
2021-11-01 15:46:25 -0700

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="volvooncall",
-    version="0.9.0",
+    version="0.9.1",
     description="Communicate with VOC",
     url="https://github.com/molobrakos/volvooncall",
     license="Unlicense",
--- volvooncall/volvooncall.py
+++ volvooncall/volvooncall.py
@@ -21,7 +21,7 @@ from .util import (
     read_config,
 )
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 _LOGGER = logging.getLogger(__name__)
 
