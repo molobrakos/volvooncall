ba89547e8c103ce06737f684b6544f025e0122dc
Jonathan Keslin <github@jonathankeslin.com>
2022-03-09 09:28:15 -0800

    Migrate to setup.cfg, clean up requirements, bump version

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-#!/usr/bin/python
-
 from setuptools import setup
-
-setup(
-    name="volvooncall",
-    version="0.9.2",
-    description="Communicate with VOC",
-    url="https://github.com/molobrakos/volvooncall",
-    license="Unlicense",
-    author="Erik",
-    author_email="error.errorsson@gmail.com",
-    scripts=["voc"],
-    packages=["volvooncall"],
-    install_requires=list(open("requirements.txt").read().strip().split("\n")),
-    extras_require={
-        "console": ["certifi", "docopt", "geopy>=1.14.0"],
-        "mqtt": list(open("requirements_mqtt.txt").read().strip().split("\n"))
-    },
-    python_requires=">=3.8"
-)
+if __name__ == '__main__':
+    setup()
