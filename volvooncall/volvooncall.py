molobrakos/volvooncall

commit f4ebbf8b0cc15ee0b71b163d68b05736660e408b

fix log spelling and give full traceback

volvooncall/volvooncall.py
@@ -79,7 +79,7 @@ class Connection:
                 return res
         except Exception as error:
             _LOGGER.warning(
-                "Failure when communcating with the server: %s", error
+                "Failure when communicating with the server: %s", error, exc_info=True
             )
             raise
 

commit 6216207debb35ef483299bc9a390fbbda1f56124

fixed linter error introduced in commit f4ebbf8

volvooncall/volvooncall.py
@@ -79,7 +79,9 @@ class Connection:
                 return res
         except Exception as error:
             _LOGGER.warning(
-                "Failure when communicating with the server: %s", error, exc_info=True
+                "Failure when communicating with the server: %s",
+                error,
+                exc_info=True,
             )
             raise
 